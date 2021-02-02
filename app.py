2b4b2c2de20e651bf47b2f4416dfa14f6a5b4cb3
porntavee-ball <porntavee.ball@g.swu.ac.th>
2021-02-02 13:38:57 +0700

    HW1

---
+++ app.py
@@ -0,0 +1,113 @@
+from urllib import parse
+from flask import Flask,render_template,request
+
+import requests
+
+
+from urllib.parse import quote
+from urllib.request import urlopen
+import json
+
+import socket
+
+app = Flask(__name__)
+
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&APPID={1}"
+WEATHER_KEY = '537e3f5fae93a2a6b7cd4c1598d8c7dc'
+
+NEWS_URL = "https://newsapi.org/v2/top-headlines?q={0}&apiKey={1}"
+NEWS_KEY = '49f02d3be61e45c286966d46bc49d313'
+
+@app.route('/')
+@app.route('/index')
+def index():
+    city = request.args.get('city')
+
+    if not city:
+        res = requests.get('https://ipinfo.io/')
+        city = res.json()['city']
+    
+    news = getCovidNews()
+
+    weather = get_weather(city,WEATHER_KEY)
+    return render_template('index.html',weather=weather,news=news,title="Index")
+
+
+@app.route('/news')
+def news():
+    tag = request.args.get('search_news')
+
+    if not tag:
+        tag = 'cat'
+
+    news = searchNews(tag,NEWS_KEY)
+    return render_template('news.html', news=news,title="News")
+
+
+
+@app.route('/aboutme')
+def aboutme():
+    return render_template('aboutme.html',title="About Me")
+
+def searchNews(tag,API_KEY):
+    query = quote(tag)
+    url = NEWS_URL.format(query, API_KEY)
+    print(url)
+    data = urlopen(url).read()
+    parsed = json.loads(data)
+    news = parsed.get('articles')
+    
+    return news
+    
+
+    
+def getCovidNews():
+    url = "https://newsapi.org/v2/top-headlines?q=covid&page=1&apiKey=49f02d3be61e45c286966d46bc49d313"
+    res = requests.get(url)
+    news = res.json()
+    list_news = list()
+    
+    for i in range(0,5):
+        list_news.append(news['articles'][i])
+    
+    return list_news
+
+
+
+
+def get_weather(city,API_KEY):
+    query = quote(city)
+    print(query)
+    url = WEATHER_URL.format(query, API_KEY)
+    print(url)
+    data = urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+
+    if parsed.get('weather'):
+
+        description = parsed['weather'][0]['description']
+        temperature = parsed['main']['temp']
+        city = parsed['name']
+        icon = parsed['weather'][0]['icon']
+        country = parsed['sys']['country']
+        wind = parsed['wind']['speed']
+        humidity = parsed['main']['humidity']
+        pressure = parsed['main']['pressure']
+        
+        weather = {'description': description,
+                    'temperature': temperature,
+                    'city': city,
+                    'country': country,
+                    'icon': icon,
+                    'wind': wind,
+                    'humidity': humidity,
+                    'pressure': pressure
+                    }
+
+    return weather
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,use_reloader=True)
